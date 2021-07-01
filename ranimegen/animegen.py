4356bcc951b0370775f64e5018f09598602de2aa
Harukomaze <85628915+Harukomaze@users.noreply.github.com>
2021-07-01 11:58:08 -0400

    Update animegen.py

--- ranimegen/animegen.py
+++ ranimegen/animegen.py
@@ -13,6 +13,6 @@ class RandomAnime:
         categories = ['1/Action','2/Adventure','4/Comedy','6/Demons','9/Ecchi', '8/Drama','10/Fantasy','11/Game','36/Slice_of_Life','16/Magic','10/Fantasy','27/Shounen','7/Mystery']
         image = await get_anime(categories)
         loop = asyncio.get_event_loop()
-        anime_name_final = await loop.run_in_executor(None, some, image)
+        anime_name_final = await loop.run_in_executor(None, parse_html, image)
         main_res = await get_info(anime_name_final)
         return main_res
