0a0b8aace70e133cbd3efa7e41af4fae9c76b8b9
Harukomaze <85628915+Harukomaze@users.noreply.github.com>
2021-07-01 11:38:34 -0400

    efficient generator

--- ranimegen/workers.py
+++ ranimegen/workers.py
@@ -1,6 +1,6 @@
 import random
 import aiohttp
-
+from bs4 import BeautifulSoup
 
 async def get_anime(categories):
     async with aiohttp.ClientSession() as session:
@@ -14,3 +14,19 @@ async def get_info(name):
                  async with cs.get(f'https://kitsu.io/api/edge/anime?filter[text]={name}') as r:
                         result = await r.json()
     return result
+
+
+
+def parse_html(image):
+    anime_list = []
+    soup = BeautifulSoup(image, 'lxml')
+    for link in soup.find_all('a'):
+            anime_list.append(link.get('href'))
+    actual_list_reduced = anime_list[83:300]
+    anime_name_list = [ anime for anime in actual_list_reduced if "authorize" not in anime and "login" not in anime and "genre" not in anime and "producer" not in anime and "episode" not in anime and "video" not in anime]
+    some_anime_link = random.choice(anime_name_list)
+    correct_anime_name = some_anime_link.split('/')[-1].split('_')
+    anime_name_final = ""
+    for elements in correct_anime_name:
+        anime_name_final += f"{elements} "
+    return anime_name_final
